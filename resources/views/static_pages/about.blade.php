43a5c88560b33ba634ea01794b0a44748f608d5a
rob <rob@robdeMac-mini.local>
2022-04-10 23:12:51 +0800

    模版切分

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>App</title>
-</head>
-<body>
+@extends('layouts.default')
+@section('title', '关于页')
+@section('content')
   <h1>关于页</h1>
-</body>
-</html>
+@endsection
--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>App</title>
-</head>
-<body>
+@extends('layouts.default')
+@section('title', '帮助页')
+@section('content')
   <h1>帮助页</h1>
-</body>
-</html>
+@endsection
